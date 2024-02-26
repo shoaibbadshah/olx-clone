30de772753635ba7e9ba7d25d5938004288e80d2
[Haris Khan] <hariskpknsr@gmail.com>
2024-02-26 22:18:13 +0500

    build created

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,5 +1,5 @@
 import connectDB from "@/lib/db";
-import ProductModel from "@/schema/schema";
+import ProductModel from "@/lib/schema/schema";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,15 +1,17 @@
 // lib/db.js
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 dotenv.config();
 
-try {
-  mongoose.connect(process.env.MongoURL, {
-    useUnifiedTopology: true,
-  });
-  console.log("database connection established");
-} catch (error) {
-  throw new Error("Error connecting to the database");
-}
-
+const connectDB = async () => {
+  // if (mongoose.connections[0].readyState) return;
+  try {
+    mongoose.connect(process.env.MongoURL, {
+      useUnifiedTopology: true,
+    });
+    console.log("database connection established");
+  } catch (error) {
+    throw new Error("Error connecting to the database");
+  }
+};
 export default connectDB;
