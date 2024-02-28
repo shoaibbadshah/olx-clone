ed307b3e0d35c063b2c97324e52943d282be6ce8
[Haris Khan] <hariskpknsr@gmail.com>
2024-02-28 19:23:21 +0500

    conflict resolved

--- next.config.js
+++ next.config.js
@@ -1,24 +1,13 @@
-<<<<<<< Updated upstream
-const { withForms } = require('@tailwindcss/forms');
-
-=======
 const { withForms } = require("@tailwindcss/forms");
-const path = require("path");
->>>>>>> Stashed changes
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Other configurations...
 
   webpack: (config, { isServer }) => {
     if (!isServer) {
-<<<<<<< Updated upstream
-      config.resolve.alias['@heroicons/react'] = '@heroicons/react/outline';
-      config.resolve.alias['@heroicons/react/solid'] = '@heroicons/react/solid';
-=======
       config.resolve.alias["@heroicons/react"] = "@heroicons/react/outline";
-      config.resolve.alias["@"] = path.join(__dirname, "src");
       config.resolve.alias["@heroicons/react/solid"] = "@heroicons/react/solid";
->>>>>>> Stashed changes
     }
 
     return config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,16 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import Testimonial from '@/components/Card';
-import Carousel from '@/components/Carousel';
-import Category from '@/components/Category';
-=======
-//@ts-ignore
 import React from "react";
 import Testimonial from "@/components/Card";
 import Carousel from "@/components/Carousel";
 import Category from "@/components/Category";
-import styles from "./globals.css";
->>>>>>> Stashed changes
 
 type Props = {};
 
 const Page = (props: Props) => {
   return (
-<<<<<<< Updated upstream
     <div>
-      <Carousel/>
-      <Category/>
-=======
-    <div className="mb-4">
       <Carousel />
       <Category />
->>>>>>> Stashed changes
       <Testimonial />
-      
-  
     </div>
   );
 };
